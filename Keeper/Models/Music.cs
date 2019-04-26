543c30a8e686b7e62e7afe9217a40559b04c6b77
Komil <library.dll@mail.ru>
2019-04-26 15:10:06 +0500

    Add project files.

---
+++ Keeper/Models/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keeper.Models
+{
+    public class Music
+    {
+		public int Id { get; set; }
+		public string Singer { get; set; }
+		public string SongName { get; set; }
+		public string SongTime { get; set; }
+	}
+}
---
+++ Keeper/ViewModels/ShellViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Caliburn.Micro;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Keeper.ViewModels
+{
+    class ShellViewModel :Screen
+    {
+		private BindableCollection<string> _musicsItemSource = new BindableCollection<string> ();
+
+		public BindableCollection<string> MusicsItemSource
+		{
+			get { return _musicsItemSource; }
+			set { _musicsItemSource = value; NotifyOfPropertyChange(() => MusicsItemSource); }
+		}
+
+
+		public void	CloseApplication()
+		{
+			this.TryClose();
+		}
+
+		public void InsertMusic()
+		{
+			OpenFileDialog open = new OpenFileDialog
+			{
+				Title = "Select Music",
+				Filter = "Music |*.mp3"
+			};
+
+			if(open.ShowDialog()==true)
+			{
+				foreach (var mus in open.FileNames)
+				{
+					MusicsItemSource.Add(Path.GetFileNameWithoutExtension(mus));
+				}
+			}
+
+
+		}
+	}
+}
